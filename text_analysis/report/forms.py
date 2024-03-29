5693b0554d3a1a19ff9d7facb5ade676355d3193
Roman Ivanov <sitdoff@gmail.com>
2024-03-29 09:49:02 +0300

    add file extension validation

--- text_analysis/report/forms.py
+++ text_analysis/report/forms.py
@@ -1,7 +1,12 @@
 from django import forms
+from django.core.validators import FileExtensionValidator
 
 
 class UploadFileForm(forms.Form):
     file = forms.FileField(
-        label="Файл", required=True, allow_empty_file=False, widget=forms.FileInput(attrs={"class": "form-control"})
+        label="Файл",
+        required=True,
+        allow_empty_file=False,
+        widget=forms.FileInput(attrs={"class": "form-control"}),
+        validators=[FileExtensionValidator(allowed_extensions=["txt"])],
     )
